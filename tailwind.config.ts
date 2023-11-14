29a6a4fb565bcf36e603b5f0fb2be588e0db5cae
Viktor Kucher <ipzk221_kvo@student.ztu.edu.ua>
2023-11-14 12:56:39 +0200

    changed config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
-import type { Config } from "tailwindcss";
-import withMT from "@material-tailwind/react/utils/withMT";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = withMT({
+module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,4 +11,3 @@ const config: Config = withMT({
   },
   plugins: [],
 });
-export default config;
